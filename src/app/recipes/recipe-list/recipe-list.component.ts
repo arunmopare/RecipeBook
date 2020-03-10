72f7a47fe66fec421dd543dd9fd59d535a2d5382
arunmopare <arunmopare546@gmail.com>
2020-03-10 12:19:04 +0530

    Added model recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from '../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recepies: Recipe[] = [
+  new Recipe ('a test recipe', 'This is simply a text', 'https://www.ambitiouskitchen.com/wp-content/uploads/2018/07/jerkskillet-3-725x725.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
